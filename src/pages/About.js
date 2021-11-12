81316c00f418c50ade126004d48591b30c9f3b70
TSav <26318217+CeasersCipher@users.noreply.github.com>
2021-11-11 19:17:07 -0500

    main

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In pellentesque massa placerat duis ultricies lacus. Mauris sit amet massa vitae tortor condimentum lacinia quis. Non consectetur a erat nam. Mattis ullamcorper velit sed ullamcorper morbi tincidunt ornare. Ornare massa eget egestas purus viverra. 
+      </p>
+    </section>
+  )
+}
+
+export default About
---
+++ src/pages/Alcohols.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AlcoholPick from '../components/AlcoholPick'
+import TypeOfAlcohol from '../components/TypeOfAlcohol'
+
+const Alcohols = () => {
+  return (
+    <main>
+      <AlcoholPick/>
+      <TypeOfAlcohol/>
+    </main>
+  )
+}
+
+export default Alcohols
